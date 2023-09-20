7d227e4f1f74fe486f0f5a91945265cc2246fc54
79998 <lekha-fadeev@mail.ru>
2023-09-20 22:13:34 +0300

    коммит

--- src/main/java/pro/sky/skayprodemo/controller/MathQuestionController.java
+++ src/main/java/pro/sky/skayprodemo/controller/MathQuestionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
+
 @RestController
 @RequestMapping("/exam/math")
 public class MathQuestionController {
--- src/main/java/pro/sky/skayprodemo/exceptions/MathQuestionNotAllowed.java
+++ src/main/java/pro/sky/skayprodemo/exceptions/MathQuestionNotAllowed.java
@@ -3,6 +3,8 @@ package pro.sky.skayprodemo.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
+
 @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
 public class MathQuestionNotAllowed extends RuntimeException {
 }
